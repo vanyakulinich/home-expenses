ecc1e8f86cb35ba5f0cf4dd0dfce9f6eb6a00196
vanyakulinich <kulinichvanya@gmail.com>
2018-07-16 22:33:52 +0300

    dashboard carcas

--- frontend/src/routes/dashboard.jsx
+++ frontend/src/routes/dashboard.jsx
@@ -21,55 +21,55 @@ const dashboardRoutes = [
     icon: Dashboard,
     component: DashboardPage
   },
-  // {
-  //   path: "/user",
-  //   sidebarName: "User Profile",
-  //   navbarName: "Profile",
-  //   icon: Person,
-  //   component: UserProfile
-  // },
-  // {
-  //   path: "/table",
-  //   sidebarName: "Table List",
-  //   navbarName: "Table List",
-  //   icon: ContentPaste,
-  //   component: TableList
-  // },
-  // {
-  //   path: "/typography",
-  //   sidebarName: "Typography",
-  //   navbarName: "Typography",
-  //   icon: LibraryBooks,
-  //   component: Typography
-  // },
-  // {
-  //   path: "/icons",
-  //   sidebarName: "Icons",
-  //   navbarName: "Icons",
-  //   icon: BubbleChart,
-  //   component: Icons
-  // },
-  // {
-  //   path: "/maps",
-  //   sidebarName: "Maps",
-  //   navbarName: "Map",
-  //   icon: LocationOn,
-  //   component: Maps
-  // },
-  // {
-  //   path: "/notifications",
-  //   sidebarName: "Notifications",
-  //   navbarName: "Notifications",
-  //   icon: Notifications,
-  //   component: NotificationsPage
-  // },
-  // {
-  //   path: "/upgrade-to-pro",
-  //   sidebarName: "Upgrade To PRO",
-  //   navbarName: "Upgrade To PRO",
-  //   icon: Unarchive,
-  //   component: UpgradeToPro
-  // },
+  {
+    path: "/user",
+    sidebarName: "User Profile",
+    navbarName: "Profile",
+    icon: Person,
+    component: UserProfile
+  },
+  {
+    path: "/table",
+    sidebarName: "Table List",
+    navbarName: "Table List",
+    icon: ContentPaste,
+    component: TableList
+  },
+  {
+    path: "/typography",
+    sidebarName: "Typography",
+    navbarName: "Typography",
+    icon: LibraryBooks,
+    component: Typography
+  },
+  {
+    path: "/icons",
+    sidebarName: "Icons",
+    navbarName: "Icons",
+    icon: BubbleChart,
+    component: Icons
+  },
+  {
+    path: "/maps",
+    sidebarName: "Maps",
+    navbarName: "Map",
+    icon: LocationOn,
+    component: Maps
+  },
+  {
+    path: "/notifications",
+    sidebarName: "Notifications",
+    navbarName: "Notifications",
+    icon: Notifications,
+    component: NotificationsPage
+  },
+  {
+    path: "/upgrade-to-pro",
+    sidebarName: "Upgrade To PRO",
+    navbarName: "Upgrade To PRO",
+    icon: Unarchive,
+    component: UpgradeToPro
+  },
   {
     path: "/signin",
     sidebarName: "Sign In",
