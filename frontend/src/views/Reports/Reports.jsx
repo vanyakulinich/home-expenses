f7a7a01c0723d26a2da7ae2fc64fc1d4b1ed9597
vanyakulinich <kulinichvanya@gmail.com>
2018-07-17 10:54:24 +0300

    reports page carcas

--- frontend/src/views/Reports/Reports.jsx
+++ frontend/src/views/Reports/Reports.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
 import Card from "components/Card/Card.jsx";
 import CardHeader from "components/Card/CardHeader.jsx";
 import CardBody from "components/Card/CardBody.jsx";
 import Table from "components/Table/Table.jsx";
+import Button from 'components/CustomButtons/Button.jsx';
+import {ChevronLeft, ChevronRight} from "@material-ui/icons";
 
-const Reports = ()=>{
+const styles = {
+    dateNav: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    }
+}
+
+const Reports = (props)=>{
+    const date = new Date().toLocaleDateString() 
+    const {classes} = props
     return (
         <Card>
             <CardHeader color='info'>
@@ -12,6 +25,21 @@ const Reports = ()=>{
                 <h5>Here is some expenses reports</h5>         
             </CardHeader>
             <CardBody>
+                <div className={classes.dateNav}>
+                    <h5>{date}</h5>
+                    <div>
+                        <Button color='primary'>
+                            <ChevronLeft/>
+                        </Button>
+                        <Button color='primary'>
+                            <ChevronRight/>
+                        </Button>
+                        <Button color='primary'>DAY</Button>
+                        <Button color='primary'>WEEK</Button>
+                        <Button color='primary'>MONTH</Button>
+                        <Button color='primary'>PERIOD</Button>
+                    </div>
+                </div>
                 <Table
                     tableHeaderColor="primary"
                     tableHead={["Category", "Expenses value, UAH"]}
@@ -26,4 +54,4 @@ const Reports = ()=>{
 }
 
 
-export default Reports;
+export default withStyles(styles)(Reports);
