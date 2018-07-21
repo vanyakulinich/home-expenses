081117766e48a9878888ee2d2d05c49996b8c9f7
vanyakulinich <kulinichvanya@gmail.com>
2018-07-21 14:19:29 +0300

    removed useless cod

--- backend/database/models.js
+++ backend/database/models.js
@@ -24,45 +24,10 @@ const CategoryModel = mongoose.model('Categories', CategorySchema);
 const UserModel = mongoose.model('Users', UserSchema);
 
 
-
-
-
-// const UserModel = mongoose.model('Users', UserSchema);
-
-// // unverified user schema and model
-// const UnverifiedUsersSchema = new Schema ({
-//     email: String,
-//     pass: String,
-//     verifyKey: String
-// })
-// const UnverifiedUsersModel = mongoose.model('VerifyUsers', UnverifiedUsersSchema)
-
-
-
-// // single category schema and model
-// const SingleCategorySchema = new Schema({
-//     name: String,
-//     value: Number,
-//     date: {type: Date, default: new Date().toLocaleDateString()}, // date format is for change
-//     children: Boolean,
-//     parent: Boolean,
-// })
-// const SingleCategoryModel = mongoose.model('Single Categories', SingleCategorySchema)
-
-// const UserDataSchema = new Schema({
-//     user: String,
-//     categories: [SingleCategorySchema]
-// })
-
-// const UserDataModel = mongoose.model('UsersData', UserDataSchema)
-
 // exported models
 const models = {
     UserModel,
     CategoryModel,
-    // UnverifiedUsersModel,
-    // SingleCategoryModel,
-    // UserDataModel
 }
 
 module.exports = models;
