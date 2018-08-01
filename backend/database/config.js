1a3a2498e93d686101c9b4fc5e9e2e1073e4d818
vanyakulinich <kulinichvanya@gmail.com>
2018-08-01 20:03:57 +0300

    config

--- backend/database/config.js
+++
@@ -1,3 +0,0 @@
-const url = 'mongodb://clientUser:klopik@lunch-shard-00-00-8j8sl.mongodb.net:27017,lunch-shard-00-01-8j8sl.mongodb.net:27017,lunch-shard-00-02-8j8sl.mongodb.net:27017/test?ssl=true&replicaSet=lunch-shard-0&authSource=admin&retryWrites=true'
-
-module.exports= url;
